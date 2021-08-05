92b4170774ea53133d1509d11ed19b5fd343e4bd
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-08-05 16:12:57 -0300

    setting up scss

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}" />
     <title>William Souza - Desenvolvedor Fullstack</title>
 </head>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Contato</a></li>
+                <li><a href="#">Portfolio</a></li>
+
+            </ul>
+        </nav>
+    </header>
     <body>
         @yield('content')
     </body>
