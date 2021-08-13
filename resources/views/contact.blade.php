a8ce87628d07c4370057c0f4e1019e90ca1e6c54
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-08-13 14:44:36 -0300

    working on contacts page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,46 @@
 @extends('layouts.site')
 
 @section('content')
-esta e a pagina de  contato
+<section class="contact">
+    <div class="message">
+        <p>Envie sua mensagem, entre em contato!!!</p>
+        <form class="contact-form" action="post">
+            <div class="field">
+                <label for="nome">Nome</label><br>
+                <input type="text" id="nome" />
+            </div>
+            <div class="field">
+                <label for="email">E-mail</label><br>
+                <input type="text" id="email" />
+            </div>
+            <div>
+                <label for="mensagem">Mensagem</label><br>
+                <textarea id="mensagem"></textarea>
+            </div>
+            <button type="submit">Enviar Mensagem</button>
+        </form>
+    </div>
+    <div class="links">
+        <div class="link-item">
+            <img src="#" alt="zap" />
+            <p>+55 21 981008743</p>
+        </div>
+        <div class="link-item">
+            <img src="#" alt="end" />
+            <p>Rodovia Amaral Peixoto, km 80</p>
+        </div>
+        <div class="link-item">
+            <img src="#" alt="email" />
+            <p>williamguilhermesouza@gmail.com</p>
+        </div>
+        <div class="link-item">
+            <img src="#" alt="github" />
+            <p>https://github.com/williamguilhermesouza</p>
+        </div>
+        <div class="link-item">
+            <img src="#" alt="linkedin" />
+            <p>https://www.linkedin.com/in/william-guilherme-45540a171</p>
+        </div>
+    </div>
+</section>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,5 +12,5 @@
         <img class="profile-pic" src="{{asset('images/profile-pic.jpg')}}" />
         <p class="profile-description">Desenvolvedor Fullstack, apaixonado pela área de desenvolvimento e envolvido no estudo de tecnologia e área de TI desde 2003. Atualmente trabalhando na área de desenvolvimento, especializado na idealização, design, prototipagem e implementação de sistemas web, desde o frontend até o backend e infraestrutura envolvida. Atuo buscando prover todos os elementos necessários para as soluções de TI idealizadas pelos clientes, com o fim de atender todos os requisitos levantados e idealização da interface com maior fidelidade possivel.</p>
     </div>
-    </section>
+</section>
 @endsection
