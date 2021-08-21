ee4275b6394031a91b893a46d8cc3eb1248e6e41
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-08-21 19:32:30 -0300

    working on portfolio page

--- resources/views/admin-home.blade.php
+++ resources/views/admin-home.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="content">
-    <p>FIRSTSSSS</p>
+<div class="admin-home">
+    <a href="#">Novo Item</a>
+    <ul class="post-list">
+        <li>
+            <div class="post-title">
+                <h2>Titulo</h2>
+                <button><img src="#" alt="edit"></button>
+                <button><img src="#" alt="excluir"></button>
+            </div>
+            <br />
+            <img src="#" alt="image">
+            <p>Esta API tem como objetivo realizar as interações necessárias com um banco de dados PostgreSQL, sendo capaz de criar registros de Alunos e de Endereços, estes últimos associados aos alunos. Nesse projeto foi utilizada a arquitetura no modelo REST, e ainda a implementação do modelo GraphQL, mantendo o modelo REST, ou seja, sendo capaz de utilizar os dois modelos para interagir com o Banco.
+
+Como framework para estrutura da API foi utilizado o NestJS, utilizado para construção de aplicações backend eficientes e escaláveis. Como linguagem, foi utilizado o Typescript aplicando elementos de programação funcional. Para implementação do projeto também foi criada uma image Docker (imagem que empacota a aplicação) e também um arquivo do Docker Compose com o qual se pode levantar a aplicação junto com seu banco de dados de uma só vez, de maneira prática.</p>
+            <p>Github: link</p>
+        </li>
+    </ul>
 </div>
 @endsection
--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.site')
 
 @section('content')
-esta e a pagina de  portfolio
+<h1 class="portfolio-title">Portfolio</h1>
+<ul class="post-list">
+        <li>
+            <div class="post-title">
+                <h2>Titulo</h2>
+            </div>
+            <br />
+            <img src="#" alt="image">
+            <p>Esta API tem como objetivo realizar as interações necessárias com um banco de dados PostgreSQL, sendo capaz de criar registros de Alunos e de Endereços, estes últimos associados aos alunos. Nesse projeto foi utilizada a arquitetura no modelo REST, e ainda a implementação do modelo GraphQL, mantendo o modelo REST, ou seja, sendo capaz de utilizar os dois modelos para interagir com o Banco.
+
+Como framework para estrutura da API foi utilizado o NestJS, utilizado para construção de aplicações backend eficientes e escaláveis. Como linguagem, foi utilizado o Typescript aplicando elementos de programação funcional. Para implementação do projeto também foi criada uma image Docker (imagem que empacota a aplicação) e também um arquivo do Docker Compose com o qual se pode levantar a aplicação junto com seu banco de dados de uma só vez, de maneira prática.</p>
+            <p>Github: <a href="#">LINK</a></p>
+        </li>
+    </ul>
 @endsection
